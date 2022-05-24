227596b70e211b1b1b621cc165e2a2daadccf074
abdellah7117 <alaouiabdellah711@gmail.com>
2022-05-24 17:52:38 +0100

    add admin controller

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,23 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using GestionEmploye.Models;
+
+namespace GestionEmploye.Controllers;
+
+public class AdminController : Controller
+{
+    private readonly AppContext _context;
+
+    public AdminController(AppContext context)
+    {
+        _context = context;
+    }
+
+
+    public IActionResult Index() {
+        ViewData["TotalEmployees"] = _context.Employe.Count();
+        // ViewBag.TotalEmployees ??= -1;
+
+        return View();
+    }
+}
