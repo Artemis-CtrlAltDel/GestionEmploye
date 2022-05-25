d9f195c5e2dafeabe9cbbbf0af8714e7ef1bc4ca
abdellah7117 <alaouiabdellah711@gmail.com>
2022-05-25 12:54:26 +0100

    fetch data 2

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -17,8 +17,10 @@ public class AdminController : Controller
     public IActionResult Index() {
         ViewData["TotalEmployees"] = _context.Employe.Count();
         ViewData["TotalDemandeConges"] = _context.Conge.Where(m=> m.Status == "Pending").Count();
+        ViewData["TotalSalarie"] = _context.Salary.Where(m=> m.Month == DateTime.Now.Month).Count();
         ViewData["Employees"] = _context.Employe.Take(10).ToList();
         ViewData["PendingConges"] = _context.Conge.Where(m => m.Status == "Pending").Take(10).ToList();
+        ViewData["CurrentConges"] = _context.Conge.Where(m => m.Status == "Accepted" && m.Date < DateTime.Now && m.Date.AddDays(m.Duration) > DateTime.Now).Take(10).ToList();
         ViewData["Salaries"] = _context.Salary.Take(10).ToList();
 
         return View();
