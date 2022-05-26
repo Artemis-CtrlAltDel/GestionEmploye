a4f29c34d6b81217cc7e201486359102b267c8d1
bruh <test.test@test.com>
2022-05-26 20:22:23 +0100

    nigga bliz

--- Controllers/EmployeesController.cs
+++ Controllers/EmployeesController.cs
@@ -33,7 +33,7 @@ namespace GestionEmploye.Controllers
                 return NotFound();
             }
 
-            var employe = await _context.Employe.Include(nameof(Person))
+            var employe = await _context.Employe.Include(nameof(Person)).Include(m=> m.Conges).Include(m=> m.Salaries)
                 .FirstOrDefaultAsync(m => m.Id == id);
             
             if (employe == null)
