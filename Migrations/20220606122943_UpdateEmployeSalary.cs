8c5dd8a1656b39080b2f00e5f7c59803e01ae9d1
bruh <test.test@test.com>
2022-06-23 20:00:27 +0100

    last commit

---
+++ Migrations/20220606122943_UpdateEmployeSalary.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GestionEmploye.Migrations
+{
+    public partial class UpdateEmployeSalary : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
